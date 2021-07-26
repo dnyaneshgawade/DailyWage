d95960d918cee4c66447a8bdf19a260321a26783
dnyana <dnyaneshgawade903@gmail.com>
2021-07-26 17:52:48 +0530

    [Dnyanesh] add : check monthly wage

--- DailyWageProblem/EmpWage.cs
+++ DailyWageProblem/EmpWage.cs
@@ -9,6 +9,7 @@ namespace DailyWageProblem
         const int WAGE_PER_HR = 20;
         const int IS_FULL_TIME = 0;
         const int IS_PART_TIME = 1;
+        const int WORKING_DAYS = 20;
         int payment = 0;
         int workHr;
 
@@ -30,7 +31,7 @@ namespace DailyWageProblem
                     Console.WriteLine("employee is absent");
                     break;
             }
-            payment = WAGE_PER_HR * workHr;
+            payment = WAGE_PER_HR * workHr * WORKING_DAYS;
             Console.WriteLine("Payment is:" + payment);
         }
     }
