ff0ba3ec61e555feb18d6d73734080e56b4aecfd
dnyana <dnyaneshgawade903@gmail.com>
2021-07-26 17:38:49 +0530

    [Dnyanesh] add : check part time daily wage

--- DailyWageProblem/EmpWage.cs
+++ DailyWageProblem/EmpWage.cs
@@ -7,11 +7,11 @@ namespace DailyWageProblem
     public class EmpWage
     {
         public const int WAGE_PER_HR = 20;
-        public const int FULL_TIME_HR = 8;
+        public const int PART_TIME_HR = 4;
         public void Wage()
         {   
-            int dailyWage = WAGE_PER_HR * FULL_TIME_HR;
-            Console.WriteLine("daily wage=" + dailyWage);
+            int dailyWage = WAGE_PER_HR * PART_TIME_HR;
+            Console.WriteLine("daily wage for part time employee =" + dailyWage);
         }
     }
 }
