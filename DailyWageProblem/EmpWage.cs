03a6a05b14366bbc6d07fe26871b11e613567b1c
dnyana <dnyaneshgawade903@gmail.com>
2021-07-26 17:34:14 +0530

    [Dnyanesh] add : check daily wage

--- DailyWageProblem/EmpWage.cs
+++ DailyWageProblem/EmpWage.cs
@@ -6,13 +6,12 @@ namespace DailyWageProblem
 {
     public class EmpWage
     {
+        public const int WAGE_PER_HR = 20;
+        public const int FULL_TIME_HR = 8;
         public void Wage()
-        {
-            Random ran = new Random();
-            int attend = ran.Next(0, 2);
-            if (attend == 0)
-                Console.WriteLine("Employee is Present");
-
+        {   
+            int dailyWage = WAGE_PER_HR * FULL_TIME_HR;
+            Console.WriteLine("daily wage=" + dailyWage);
         }
     }
 }
