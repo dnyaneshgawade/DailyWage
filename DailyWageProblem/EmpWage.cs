34b25f0bb07373950553a93dbd91f384a32ba0a9
dnyana <dnyaneshgawade903@gmail.com>
2021-07-27 16:47:53 +0530

    [Dnyanesh] add : for multiple company wage

--- DailyWageProblem/EmpWage.cs
+++ DailyWageProblem/EmpWage.cs
@@ -6,26 +6,37 @@ namespace DailyWageProblem
 {
     public class EmpWage
     {
-        const int WAGE_PER_HR = 20;
         const int IS_FULL_TIME = 0;
         const int IS_PART_TIME = 1;
-        const int WORKING_DAYS = 20;
-        public const int PART_TIME_HR = 4;
-        public const int FULL_TIME_HR = 8;
-        int fPayment;
-        int hPayment;
+        public static int payment = 0;
+        public static int workHr;
+        public static int totalDays=0;
+        public static int totalHrs=0;
 
-        public void Wage()
+        public static int Wage(string company, int wagePerHr, int workingDays, int maxHr)
         {
-            Console.WriteLine("using differnt classes");
-            for (int i = 1; i <= WORKING_DAYS; i++)
+            while(workingDays <= 20 || maxHr <= 100)
             {
-                int working_day = i;
-                fPayment = WAGE_PER_HR * FULL_TIME_HR * working_day;
-                Console.WriteLine("full time Payment of day" + i + " is:" + fPayment);
-                hPayment = WAGE_PER_HR * PART_TIME_HR * working_day;
-                Console.WriteLine("part time Payment of day" + i + " is:" + hPayment);
+                Random random = new Random();
+                int num = random.Next(0, 3);
+                switch (num)
+                {
+                    case IS_FULL_TIME:
+                        workHr = 8;
+                        break;
+                    case IS_PART_TIME:
+                        workHr = 4;
+                        break;
+                    default:
+                        break;
+                }
+                totalDays++;
+                totalHrs += workHr;
             }
+            
+            payment = wagePerHr * totalHrs;
+            Console.WriteLine("Total Employee Wage for company " + company + ":" + payment);
+            return payment;
         }
     }
 }
--- DailyWageProblem/Program.cs
+++ DailyWageProblem/Program.cs
@@ -7,8 +7,9 @@ namespace DailyWageProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to Daily Wage!");
-            EmpWage emp = new EmpWage();
-            emp.Wage();
+            EmpWage.Wage("facebook",12,10,50);
+            EmpWage.Wage("google", 15, 15, 90);
+            EmpWage.Wage("microsoft", 30, 20, 100);
         }
     }
 }
